53f000d019790481144f8215c5e9df005bf5e805
Swapnil Gaikwad <swpnlgkwd@hotmail.com>
2021-06-22 11:03:09 +0530

    Day2 : Static Middleware , Basic MVC understanding

---
+++ sample-app/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sample_app.Controllers
+{
+    // Controller its base class for Controller
+    // it gives the capability to HomeCtrl To Return View Or Page
+    //  ViewResult, JsonResult, PartialView
+
+    // Controller class : ControllerBase : that is used in the REST API
+
+    public class HomeController : Controller
+    {
+        // Action Methods
+        // IActionResult : It can Return Multiple Types
+        // String Page, File, JSON , Route,RedirectoAction 
+        public IActionResult Index()
+        {
+             return View(); //Page       
+        }       
+
+        public IActionResult AboutUs()
+        {
+            return View(); //Page
+        }
+
+        public IActionResult Contact()
+        {
+            return View(); //Page
+        }
+
+
+    }
+}
--- sample-app/Startup.cs
+++ sample-app/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,8 @@ namespace sample_app
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();// ACtivate all the object or service require to understand ctrl and views
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,33 +28,43 @@ namespace sample_app
             {
                 app.UseDeveloperExceptionPage();
             }
+            // Built In Middleware 
+            // Serve  Static Files
+            app.UseStaticFiles(); // Understand  wwwroot
 
-            app.Use(async (context, next) =>
+            
+            app.UseStaticFiles(new StaticFileOptions
             {
-                await context.Response.WriteAsync("First Middleware");
-                await next.Invoke();
+                FileProvider = new PhysicalFileProvider($"{env.ContentRootPath}/kashishwwwroot"),
+                RequestPath = "/files"
             });
+
+            //// Using Use and Run Method to Create Middleware
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("First Middleware");
+            //    await next.Invoke();
+            //});            
+            
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Second Middleware");
+            //    await next.Invoke();
+            //});
 
             // Circuit Breaker :  Next Middleware will not be called.
             //app.Run(async (context) =>
             //{
             //    await context.Response.WriteAsync("Broken   the Execution");
             //});
-            // Middleware2
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("Second Middleware");
-                await next.Invoke();
-            });
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                // Default URL :   http://localhost/home/index
+                endpoints.MapDefaultControllerRoute(); // routing  ..
+                
             });
         }
     }
